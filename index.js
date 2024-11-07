2687bf930768cd9f6a664c34fb9373d4d96afb28
Kyle Alvares <kyle.alv2000@gmail.com>
2024-11-07 00:37:04 -0500

    🚀 Server listens on port 8080 to handle Back4App health checks

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ require('dotenv').config();
 const fs = require('fs');
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 
+const express = require('express');
+const app = express();
+
+// Basic route to check if the app is healthy
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+// Required Port for Back4App
+app.listen(8080, () => {
+  console.log('Server running on port 8080');
+});
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
